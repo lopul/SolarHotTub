48089b6ce7ba45cbf341b13dc3c45ed5f6897a22
Lorenz Pullwitt <68168097+lopul@users.noreply.github.com>
2022-05-29 23:00:12 +0200

    Add files via upload

--- SolarHotTub.c
+++ SolarHotTub.c
@@ -1,5 +1,6 @@
 // SolarHotTub.c
 // author: Lorenz Pullwitt
+// copyright 2022
 // license: GPL 2
 // version: 0
 
@@ -8,6 +9,30 @@
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #endif
+
+static uint16_t morse_chart[91] = {
+    0, 0, 0, 0, 0, 0, 0, 0, // 0
+    0, 0, 0, 0, 0, 0, 0, 0, // 8
+    0, 0, 0, 0, 0, 0, 0, 0, // 16
+    0, 0, 0, 0, 0, 0, 0, 0, // 24
+    0, 0, 0, 0, 0, 0, 0, 0, // 32
+    0, 0, 0, 0, 0, 0, 0, 0, // 40
+    0x2AA, 0x2A9, 0x2A5, 0x295, 0x255, 0x155, 0x156, 0x15A, // 48 (0 - 7)
+    0x16A, 0x1AA, 0, 0, 0, 0, 0, 0, // 56 (8 - 9)
+    0, 0x9, 0x56, 0x66, 0x16, 0x1, 0x65, 0x1A, // 64 (A - G)
+    0x55, 0x5, 0xA9, 0x26, 0x59, 0xA, 0x6, 0x2A, // 72 (H - O)
+    0x69, 0x9A, 0x19, 0x15, 0x2, 0x25, 0x95, 0x29, // 80 (P - W)
+    0x96, 0xA6, 0x5A // 88 (X - Z)
+};
+
+static uint16_t ch2morse(uint8_t ch)
+{
+    uint16_t morse;
+    morse = 0;
+    if (ch <= 90)
+        morse = morse_chart[ch];
+    return morse;
+}
 
 int main()
 {
@@ -21,3 +46,6 @@ int main()
         sleep_ms(250);
     }
 }
+
+
+

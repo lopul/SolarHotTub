f41a2dd8968cf8756d31eb4a27e69de417f46439
Lorenz Pullwitt <68168097+lopul@users.noreply.github.com>
2022-05-29 23:53:55 +0200

    Add files via upload

--- SolarHotTub.c
+++ SolarHotTub.c
@@ -2,13 +2,39 @@
 // author: Lorenz Pullwitt
 // copyright 2022
 // license: GPL 2
-// version: 0
+// version: 4
+
+#ifdef NO_PICO_DEBUG
+// gcc -Wall -g -O0 -D NO_PICO_DEBUG -fsanitize=address SolarHotTub.c -o solarhottub
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+typedef unsigned int uint;
+static void gpio_put (uint gpio, bool value)
+{
+}
+static void gpio_init (uint gpio)
+{
+}
+static void gpio_set_dir (uint gpio, bool out)
+{
+}
+static void sleep_ms (uint32_t ms)
+{
+}
+#define PICO_DEFAULT_LED_PIN 25
+#define GPIO_OUT 1
+
+#else
 
 #include "pico/stdlib.h"
 
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #endif
+
+#endif
+
 
 static uint16_t morse_chart[91] = {
     0, 0, 0, 0, 0, 0, 0, 0, // 0
@@ -36,15 +62,39 @@ static uint16_t ch2morse(uint8_t ch)
 
 int main()
 {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    int e;
+    uint16_t morse;
+    uint8_t ch;
+    uint16_t sig;
+    char symb;
+    e = 0;
+    for (ch = 0; ch <= 90 && e == 0; ch++) {
+        morse = ch2morse(ch);
+        if (morse > 0) {
+            printf("%c = ", ch);
+            do {
+                sig = morse & 0x3;
+                switch (sig) {
+                    case 1: symb = '.'; break;
+                    case 2: symb = '-'; break;
+                    default: e = 1;
+                }
+                if (e == 0)
+                    printf("%c", symb);
+                morse >>= 2;
+            } while (morse > 0 && e == 0);
+            printf("\n");
+        }
+    }
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     while (true) {
-        gpio_put(LED_PIN, 1);
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
         sleep_ms(250);
     }
+    return e;
 }
 
 
